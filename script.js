cd9f2f19c6f98e9d13a4f65275d43a7a9f7d7a95
Gautham-Asok <gauthamasokpullur@gmail.com>
2024-08-09 15:19:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("Hello World");
+let a=1
+b=5
+alert(a+b);
+function add(x,y){
+
+    var sum=x+y;
+    return=sum;
+}
+
+var n=3;
+var m=4;
+var s= add(n,m);
+alert(s);
+function fn(){
+    var a=document.getElementById('ta').ariaValueMax;
+    document.getElementById('par').innerHTML=a;   
